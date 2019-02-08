9c126f884531771c9482a3a924023481e4093b12
Razvan <razwan.me@gmail.com>
2019-02-08 12:35:26 +0200

    git subrepo clone https://github.com/pixelgrade/components.git components
    subrepo:
      subdir:   "components"
      merged:   "bb23a20"
    upstream:
      origin:   "https://github.com/pixelgrade/components.git"
      branch:   "master"
      commit:   "bb23a20"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

---
+++ components/woocommerce/js/scripts.js
@@ -0,0 +1,24 @@
+(function($) {
+
+	// when document is ready
+	$(function() {
+
+		// show mini cart when a product is added to cart
+		$( 'body' ).not( '.woocommerce-cart' ).on( 'adding_to_cart', function() {
+			$( '.c-mini-cart' ).addClass( 'c-mini-cart--visible' );
+		});
+
+		$( '.menu-item--cart' ).on( 'click', 'a', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			$( '.c-mini-cart' ).addClass( 'c-mini-cart--visible' );
+		});
+
+		// hide mini cart when user clicks outside the mini cart or on the close button
+		$( '.c-mini-cart__overlay, .c-mini-cart__close').on( 'click', function() {
+			$( '.c-mini-cart' ).removeClass( 'c-mini-cart--visible' );
+		});
+
+	});
+
+})(jQuery);
---
+++ components/woocommerce/templates/archive-product.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all product archives.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * This template part can be overridden by copying it to a child theme or in the same theme
+ * by putting it in the root `/single.php` or in `/templates/blog/single.php`.
+ * @see pixelgrade_locate_component_template()
+ *
+ * HOWEVER, on occasion Pixelgrade will need to update template files and you
+ * will need to copy the new files to your child theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see        https://pixelgrade.com
+ * @author     Pixelgrade
+ * @package    Components/Woocommerce
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+pixelgrade_get_header();
+
+pixelgrade_render_block( 'woocommerce/archive-product' );
+
+pixelgrade_get_footer();
