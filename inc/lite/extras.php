5fe8413844383388d2d5ff333c6cb9e606a38da7
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-04-03 14:19:23 +0300

    better handling for admin pixcare notice

--- inc/lite/extras.php
+++ inc/lite/extras.php
@@ -1,14 +1,9 @@
 <?php
 
 /**
- * Theme About page logic.
+ * Admin Dashboard logic.
  */
-require pixelgrade_get_parent_theme_file_path( pixelgrade_get_theme_relative_path( __DIR__ ) . 'admin/about-page.php' ); // @codingStandardsIgnoreLines
-
-/**
- * Pixelgrade Care notice logic.
- */
-require pixelgrade_get_parent_theme_file_path( pixelgrade_get_theme_relative_path( __DIR__ ) . 'admin/pixcare-notice/notice.php' ); // @codingStandardsIgnoreLines
+require pixelgrade_get_parent_theme_file_path( pixelgrade_get_theme_relative_path( __DIR__ ) . 'admin/admin.php' ); // @codingStandardsIgnoreLines
 
 /**
  * Check if the widget is only available for the Pro version.
